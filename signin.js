7c813892d61e404756b7d43559d0fae7530f19ab
jaet0who <drjana2014@gmail.com>
2025-11-29 18:14:46 +0200

    linked the html pages using js!

--- signin.js
+++ signin.js
@@ -1,3 +1,20 @@
 //linking pages using buttons in signin.html
 //login administrator should take us to adminLogin
 //login cutsomer should take us to customerLogin
+
+//button for adminlogin: admin-btn
+//button for customerlogin: cus-btn
+
+document.getElementById("admin-btn").onclick= takeToAdminLogin;
+document.getElementById("cus-btn").onclick= takeToCustomerLogin;
+
+function takeToAdminLogin()
+{
+    window.location.href= "adminLogin.html";
+}
+
+function takeToCustomerLogin()
+{
+    window.location.href= "customerLogin.html";
+}
+
