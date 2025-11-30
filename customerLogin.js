c3d54da0536a64eeb0563e9ad40346572a962e9a
jaet0who <drjana2014@gmail.com>
2025-11-30 17:48:22 +0200

    css done for customer login, working on js

---
+++ customerLogin.js
@@ -0,0 +1,12 @@
+//check username-> must have numbers and letters, minimum is 5 characters
+//check email-> must be of a valid format
+// password-> minimum 8 characters, must include letters and numbers.
+//confirm-> must match password
+
+//->user cannot log into customerHome.html unless all criteris is met
+//-> each criteria has its own error message
+//-> ince all criteria is valid, only then will the register buttom take them to the customerHome.html page
+
+document.getElementById('reg-btn').addEventListener('click', function(event) {
+    
+});
