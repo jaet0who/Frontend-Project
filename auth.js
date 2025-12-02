bda9f17ee0f6ed2b75f6fa3192e4c88326f92c55
rokaia emad <rokaia.emad@msa.edu.eg>
2025-12-02 20:04:06 +0200

    edit inedx.html ,adminlogin.html

---
+++ auth.js
@@ -0,0 +1,22 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const role = localStorage.getItem("userRole");
+    if (role === "admin") {
+        document.getElementById("cart-link").style.display = "none";
+        document.getElementById("feedback-link").style.display = "none";
+    }
+});
+function setRole(role) {
+    localStorage.setItem("userRole", role);
+    window.location.href = "index.html";
+}
+function goToBooks() {
+    const role = localStorage.getItem("userRole");
+    if (role === "admin") {
+        window.location.href = "adimn.html";
+    }
+    else {
+        window.location.href = "Offer.html";
+    }
+}
