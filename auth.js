b0fe95931907039356e4fa0558f95d59a814065f
rokaia emad <rokaia.emad@msa.edu.eg>
2025-12-03 12:49:28 +0200

    admin-books files edit

--- auth.js
+++ auth.js
@@ -1,4 +1,24 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+    const loginBtn = document.getElementById("Login-btn");
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+    console.log("Login status:", isLoggedIn);
+
+    if (isLoggedIn === "true") {
+        loginBtn.textContent = "Logout";
+        loginBtn.onclick = function () {
+            localStorage.removeItem("isLoggedIn");
+            localStorage.removeItem("userRole");
+            window.location.href = "index.html";
+        };
+    } else {
+        loginBtn.textContent = "Login";
+        loginBtn.onclick = function () {
+            window.location.href = "signin.html";
+        };
+    }
+});
 
 document.addEventListener("DOMContentLoaded", () => {
     const role = localStorage.getItem("userRole");
@@ -9,14 +29,14 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 function setRole(role) {
     localStorage.setItem("userRole", role);
+    localStorage.setItem("isLoggedIn", "true");
     window.location.href = "index.html";
 }
 function goToBooks() {
     const role = localStorage.getItem("userRole");
     if (role === "admin") {
-        window.location.href = "adimn.html";
-    }
-    else {
+        window.location.href = "admin-Book.html";
+    } else {
         window.location.href = "Offer.html";
     }
 }
